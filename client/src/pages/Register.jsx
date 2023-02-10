0d705a9c308a70fda03c37162c846fd6d87432ad
unknown <filip.trifunovic23@gmail.com>
2023-02-10 13:46:13 +0100

    register page ui

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto">
+          <input type="text" placeholder="name" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
-            Dont have an account yet?{" "}
-            <Link className="underline text-black" to={"/register"}>
-              Register now
+            Already a member?{" "}
+            <Link className="underline text-black" to={"/login"}>
+              Login
             </Link>
           </div>
         </form>
@@ -21,4 +22,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
