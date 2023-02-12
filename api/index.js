8bb22f96c48653bc8a88bb714fe9b4bcb572ef76
unknown <filip.trifunovic23@gmail.com>
2023-02-12 13:31:00 +0100

    add photo from url

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const User = require("./models/User.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
 require("dotenv").config();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(
   cors({
@@ -89,6 +91,16 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+  res.json(newName);
 });
 
 app.listen(4000);
--- client/src/pages/PlacesPages.jsx
+++ client/src/pages/PlacesPages.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
 import Perks from "../Perks";
+import axios from "axios";
 
 const PlacesPages = () => {
   const { action } = useParams();
@@ -30,6 +31,18 @@ const PlacesPages = () => {
         {inputDescription(description)}
       </>
     );
+  }
+
+  async function addPhotoByLink(e) {
+    e.preventDefault();
+    const { data: filename } = await axios.post("/upload-by-link", {
+      link: photoLink,
+    });
+    setAddedPhotos((prev) => {
+      return [...prev, filename];
+    });
+
+    setPhotoLink("");
   }
 
   return (
@@ -90,12 +103,25 @@ const PlacesPages = () => {
                 onChange={(e) => setPhotoLink(e.target.value)}
                 placeholder="add using a link from web"
               />
-              <button className="bg-gray-200 px-4 rounded-2xl">
+              <button
+                className="bg-gray-200 px-4 rounded-2xl"
+                onClick={addPhotoByLink}
+              >
                 Add photo
               </button>
             </div>
-            <div className="mt-2 grid grid-cols-3 lg:grid-cols-6 md:grid-cols-4">
-              <button className="mt-10 flex gap-1 justify-center border-2 bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+            <div className="mt-2 gap-2 grid grid-cols-3 lg:grid-cols-6 md:grid-cols-4">
+              {addedPhotos.length > 0 &&
+                addedPhotos.map((link, index) => (
+                  <div key={index}>
+                    <img
+                      src={"http://localhost:4000/uploads/" + link}
+                      alt="photo"
+                      className="rounded-2xl"
+                    />
+                  </div>
+                ))}
+              <button className="mt-10 flex items-center gap-1 justify-center border-2 bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
