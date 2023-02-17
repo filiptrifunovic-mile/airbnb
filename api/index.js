626b57e858cddd355c9e02bef41475b166a48502
Filip Trifunovic <114927397+filiptrifunovic-mile@users.noreply.github.com>
2023-02-17 13:30:42 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,10 +23,7 @@ app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:5173",
-  })
+  cors()
 );
 mongoose.connect(process.env.MONGO_URL);
 
