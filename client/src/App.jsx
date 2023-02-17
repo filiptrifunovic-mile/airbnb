e0a39d7d6d286522a461c52b550bc0f185c821be
Filip Trifunovic <114927397+filiptrifunovic-mile@users.noreply.github.com>
2023-02-17 13:44:32 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 axios.defaults.withCredentials = true;
 
 function App() {
